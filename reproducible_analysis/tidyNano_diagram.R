b1818080bc136007d084802fa6809d442311025a
nguyens7 <nguyens7@msu.edu>
2018-03-27 17:05:04 -0400

    remove package

--- reproducible_analysis/tidyNano_diagram.R
+++ reproducible_analysis/tidyNano_diagram.R
@@ -113,15 +113,82 @@ grViz("
 
       node [fillcolor = Plum]
       
-      p [label = 'nanoShiny'];
+      p [label = 'shinySIGHT'];
       
       a->c
       a->b
       b-> d
       c->d->e->h->k->o->j
-      h->{e o i j p l}
+      h->{e o i j l}
       l->q
       q->e->i->m
       {k o}->l
+      h->p
+      
+      }")
+
+
+
+
+# Version 3
+
+grViz("
+      digraph dot {
+      
+      graph [layout = dot,
+      rankdir = LR]
+      
+      node [shape = Box,
+      style = filled,
+      color = Black,
+      fontname = Helvetica,
+      penwidth = 2.0]
+      
+      node [fillcolor = Plum]
+      
+      c [label = 'nanoimport'];
+      d [label = 'nanotidy'];
+      k [label = 'nanocount'];
+      l [label = 'nanonest'];
+      q [label = 'nanoShapiro'];
+      o [label = 'nanolyze'];
+      
+      
+      node [fillcolor= GhostWhite]    
+      
+      a [label = 'Raw Nanosight Data'];
+      b [label = 'User Cleaned Data '];
+      i [label = 'base R statistics'];
+      
+      node [fillcolor = Cyan2]
+      
+      e [label = 'dplyr'];    
+      h [label = 'tidy data'];
+      m [label = 'broom'];
+      
+      node [shape = circle,
+      style = filled,
+      color = Black,
+      fontname = Helvetica,
+      penwidth = 2.0,
+      width = 1]
+      
+      node [fillcolor = Cyan2]
+      
+      j [label = 'ggplot2'];
+      
+      node [fillcolor = Plum]
+      
+      p [label = 'shinySIGHT'];
+      
+      a->c
+      a->b
+      b-> d
+      c->d->e->h->k->o->j
+      h->{e o i j l}
+      l->q
+      i->m
+      {k o}->l
+      h->p
       
       }")
